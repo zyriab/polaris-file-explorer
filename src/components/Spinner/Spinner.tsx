561158f6c942fef2611d9e611e162cb4cf7ed261
ZyriabDsgn <flacodevworks@gmail.com>
2022-10-27 18:44:49 +0200

    fix: fixed conflicting names

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Spinner } from '@shopify/polaris';
+import { Spinner as PolarisSpinner } from '@shopify/polaris';
 
 import './Spinner.css';
 
 export function Spinner() {
   return (
     <div className="Spinner__Wrapper">
-      <Spinner accessibilityLabel="Editor loading spinner" />
+      <PolarisSpinner accessibilityLabel="Editor loading spinner" />
     </div>
   );
 }
